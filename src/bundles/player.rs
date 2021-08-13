f220c934b784da3600c3342921377a88153d852d
JMS55 <47158642+JMS55@users.noreply.github.com>
2021-08-13 00:56:58 -0400

    Only allow consecutive player movements if holding down a button

--- src/bundles/player.rs
+++ src/bundles/player.rs
@@ -1,9 +1,10 @@
 use crate::actions::{Action, Direction, MoveAction};
 use crate::bundles::SpriteBundleExt;
-use crate::components::{Actor, Damageable, GridPosition, KeepBetweenFloors, TurnGroup};
+use crate::components::{Actor, Brain, Damageable, GridPosition, KeepBetweenFloors, TurnGroup};
 use crate::world::ImmutableWorld;
 use bevy::input::Input;
 use bevy::prelude::{Bundle, Entity, KeyCode, SpriteBundle};
+use std::time::{Duration, Instant};
 
 #[derive(Bundle)]
 pub struct Player {
@@ -20,40 +21,89 @@ impl Player {
         Self {
             position: GridPosition::new(x, y),
             damageable: Damageable::new(),
-            actor: Actor::new(player_brain, TurnGroup::Player),
+            actor: Actor::new(PlayerBrain::CanMoveOnce, TurnGroup::Player),
             sprite: SpriteBundle::new("soul_spectre.png", x, y),
             kbf: KeepBetweenFloors,
         }
     }
 }
 
-fn player_brain(player_entity: Entity, world: &mut ImmutableWorld) -> Option<Box<dyn Action>> {
-    let keyboard = world.get_resource::<Input<KeyCode>>().unwrap();
-    if keyboard.pressed(KeyCode::W) {
-        MoveAction {
-            entity: player_entity,
-            direction: Direction::Up,
+#[derive(Clone)]
+enum PlayerBrain {
+    CanMoveOnce,
+    MovedOnce { when: Instant },
+    MovingMany,
+}
+
+impl PlayerBrain {
+    fn decide_action(this_entity: Entity, world: &mut ImmutableWorld) -> Option<Box<dyn Action>> {
+        let keyboard = world.get_resource::<Input<KeyCode>>().unwrap();
+        if keyboard.pressed(KeyCode::W) {
+            MoveAction {
+                entity: this_entity,
+                direction: Direction::Up,
+            }
+            .to_brain_decision_if_can_perform(world)
+        } else if keyboard.pressed(KeyCode::A) {
+            MoveAction {
+                entity: this_entity,
+                direction: Direction::Left,
+            }
+            .to_brain_decision_if_can_perform(world)
+        } else if keyboard.pressed(KeyCode::S) {
+            MoveAction {
+                entity: this_entity,
+                direction: Direction::Down,
+            }
+            .to_brain_decision_if_can_perform(world)
+        } else if keyboard.pressed(KeyCode::D) {
+            MoveAction {
+                entity: this_entity,
+                direction: Direction::Right,
+            }
+            .to_brain_decision_if_can_perform(world)
+        } else {
+            None
         }
-        .to_brain_decision_if_can_perform(world)
-    } else if keyboard.pressed(KeyCode::A) {
-        MoveAction {
-            entity: player_entity,
-            direction: Direction::Left,
+    }
+}
+
+impl Brain for PlayerBrain {
+    fn decide_action(
+        &mut self,
+        this_entity: Entity,
+        world: &mut ImmutableWorld,
+    ) -> Option<Box<dyn Action>> {
+        // Only allow consecutive movements if trying to move for at least 300ms
+        match self {
+            Self::CanMoveOnce => {
+                let action = Self::decide_action(this_entity, world);
+                if action.is_some() {
+                    *self = Self::MovedOnce {
+                        when: Instant::now(),
+                    };
+                }
+                action
+            }
+            Self::MovedOnce { when } => {
+                let action = Self::decide_action(this_entity, world);
+                if action.is_some() {
+                    if when.elapsed() >= Duration::from_millis(300) {
+                        *self = Self::MovingMany;
+                        return action;
+                    }
+                } else {
+                    *self = Self::CanMoveOnce;
+                }
+                None
+            }
+            Self::MovingMany => {
+                let action = Self::decide_action(this_entity, world);
+                if action.is_none() {
+                    *self = Self::CanMoveOnce;
+                }
+                action
+            }
         }
-        .to_brain_decision_if_can_perform(world)
-    } else if keyboard.pressed(KeyCode::S) {
-        MoveAction {
-            entity: player_entity,
-            direction: Direction::Down,
-        }
-        .to_brain_decision_if_can_perform(world)
-    } else if keyboard.pressed(KeyCode::D) {
-        MoveAction {
-            entity: player_entity,
-            direction: Direction::Right,
-        }
-        .to_brain_decision_if_can_perform(world)
-    } else {
-        None
     }
 }
